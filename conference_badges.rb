10360bbdc6cde13e51f690f35caff77dce214123
Adam Duff <j.adam.duff@gmail.com>
2018-08-12 20:58:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,8 +8,8 @@ end
 
 def assign_rooms(attendees)
   room_num = 1
-  attendees.map do |name|
-    "Hello, #{name}! You'll be assigned to room #{room_num}"
+  attendees.each do |name|
+    name = "Hello, #{name}! YOu'll be assigned to room #{room_num}!"
     room_num ++
   end
 end
