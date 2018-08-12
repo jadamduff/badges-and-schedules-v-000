98cb5b2ce84e91a82b4c6703c7a4e94fdefa7ed3
Adam Duff <j.adam.duff@gmail.com>
2018-08-12 20:45:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hellp, my name is #{name}"
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect {|attendee| badge_maker(attendee)}
+end
+
+def assign_rooms(attendees)
+  room_num = 1
+  match_badge_creator(attendees).collect do |i|
+
+  end
+end
