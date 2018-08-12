4c4a08ba6174aac0d54e9ae64c863efeee821ba1
Adam Duff <j.adam.duff@gmail.com>
2018-08-12 21:10:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,9 +7,12 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
+  room_num = 1
   attendees.each do |name|
-    name = "Hello, #{name}! You'll be assigned to room 2!"
+    name = "Hello, #{name}! You'll be assigned to room #{room_num}!"
+    room_num ++
   end
+  return attendees
 end
 
 def printer(attendees)
